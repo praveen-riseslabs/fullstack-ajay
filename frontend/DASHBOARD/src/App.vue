cd4e2acea14e0cfbe90ce0f5060575f86a50e4e7
AjayGowlikar <152581936+AjayGowlikar@users.noreply.github.com>
2023-12-21 10:01:21 +0530

    Dashboard design
    Dashboard design

---
+++ frontend/DASHBOARD/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+      <nav class="navbar navbar-expand-lg ">
+
+<div class="container-fluid">
+  <a class="navbar-brand">Navbar</a>
+
+  <form class="d-flex" role="search">
+    <i class="fa-solid me-3 fa-bell fa-xl"></i>
+    <i class="fa-solid me-3 fa-message fa-xl"></i>
+    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle me-3" height="25"
+      alt="Black and White Portrait of a Man" loading="lazy" />
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+      Dropdown
+    </a>
+    <ul class="dropdown-menu">
+      <li><a class="d-flex dropdown-item" href="#">Action</a></li>
+      <li><a class="dropdown-item" href="#">Another action</a></li>
+      <li>
+        <hr class="dropdown-divider">
+      </li>
+      <li><a class="dropdown-item" href="#">Something else here</a></li>
+    </ul>
+    <button class="btn">hey</button>
+  </form>
+</div>
+</nav> 
+  <router-view />
+</template>
+
+<style>
+@import "@/style.css";
+</style>
---
+++ frontend/DASHBOARD/src/views/DashboardView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+  </template>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+  crossorigin="anonymous">
+   
+    
+  import HelloWorld from "@/components/HelloWorld.vue";
+  
+  export default {
+    name: "DashboadView",
+    components: {
+      HelloWorld,
+    },
+  };
+  </script>
+  

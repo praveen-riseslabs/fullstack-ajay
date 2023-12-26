520f12df86c8337a3d22d385dd9d4401450f018c
AjayGowlikar <152581936+AjayGowlikar@users.noreply.github.com>
2023-12-26 17:04:23 +0530

    dashboard design with left navbar

---
+++ frontend/dashboard-frontend-only/src/views/DashboardView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Dashboard from "@/components/DashboardPage.vue";
+
+export default {
+  name: "DashboardView",
+  components: {
+    Dashboard,
+  },
+};
+</script>

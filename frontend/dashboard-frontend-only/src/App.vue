73f855058699f84947a15685eafdeb3fc68844d7
AjayGowlikar <152581936+AjayGowlikar@users.noreply.github.com>
2023-12-26 17:17:06 +0530

    Update App.vue

--- frontend/dashboard-frontend-only/src/App.vue
+++ frontend/dashboard-frontend-only/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg stats" data-bs-theme="dark">
     <div class="container">
-      <a class="navbar-brand" href="#">RISESLABS</a>
+      <a class="navbar-brand" href="#"><img src="../src/img/logo.webp" alt=""></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
